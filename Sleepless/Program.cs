f1c4631196dff9fd4dcf5206f7b01eddd9a92988
KarlDoreth <30433846+KarlDoreth@users.noreply.github.com>
2020-05-02 10:35:23 +0200

    First running Version
    It runs well!

---
+++ Sleepless/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Sleepless
+{
+    static class Program
+    {
+        /// <summary>
+        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            
+
+            SleeplessManagement SM = new SleeplessManagement();
+            Application.Run();
+        }
+
+    }
+}
---
+++ Sleepless/SleeplessManagement.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.Diagnostics;
+
+namespace Sleepless
+{
+    class SleeplessManagement
+    {
+        //Globale Objekte
+        Timer MyTimer;
+        NotifyIcon MyTray;
+
+        public SleeplessManagement()
+        {
+            inittray();
+
+            MyTimer = new Timer();
+            MyTimer.Interval = 5000;
+            MyTimer.Tick += MyTimer_Tick;
+            MyTimer.Start();
+        }
+        
+        private void MyTimer_Tick(object sender, EventArgs e)
+        {
+            CountMonitors();
+        }
+
+        //TraySteuerung
+        
+        public void inittray()
+        {
+            //Tray Icon vorbereiten
+            MyTray = new NotifyIcon();
+            MyTray.Text = "Sleepless Application";
+            MyTray.Icon = new Icon("Grey.ico");
+            MyTray.Visible = true;
+
+            //Aktion für Linksklick
+            //MyTray.Click += CountMonitors;
+
+            //Context Menü für Funktionen
+            ContextMenu CM = new ContextMenu();
+
+            CM.MenuItems.Add("Close", ExitClick);
+            CM.MenuItems.Add("Force Standby", StandbyClick);
+
+            MyTray.ContextMenu = CM;
+        }
+
+        private void ExitClick(Object sender, EventArgs e)
+        {
+            MyTray.Visible = false;
+            Application.Exit();
+        }
+
+        private static void StandbyClick(Object sender, EventArgs e)
+        {
+            var psi = new ProcessStartInfo("rundll32.exe", "powrprof.dll,SetSuspendState");
+            psi.CreateNoWindow = true;
+            psi.UseShellExecute = false;
+            Process.Start(psi);
+        }
+
+        void CountMonitors()
+        {
+            int AnzahlMonitore = 0;
+            string query = "select * from WmiMonitorBasicDisplayParams";
+            using (var wmiSearcher = new ManagementObjectSearcher("\\root\\wmi", query))
+            {
+                var results = wmiSearcher.Get();
+                AnzahlMonitore = results.Count;
+            }
+            if (AnzahlMonitore > 1)
+            {
+                //Console.WriteLine("Sleepless Mode");
+                SetEnergyScheme(0);
+                MyTray.Text = "Mode: Sleepless!";
+                MyTray.Icon = new Icon("Red.ico");
+            }
+            else
+            {
+                //Console.WriteLine("Sleep Mode");
+                SetEnergyScheme(1);
+                MyTray.Text = "Mode: Suspend!";
+                MyTray.Icon = new Icon("Green.ico");
+            }
+        }
+
+        static void SetEnergyScheme(int TheValue)
+        {
+            string EnergieSchema = "381b4222-f694-41f0-9685-ff5bb260df2e"; //Ausbalanciert
+            string Untergruppe = "4f971e89-eebd-4455-a8de-9e59040e7347"; //Netzschalter und Zuklappen
+            string Einstellung = "5ca83367-6e45-459f-a27b-476b1d01c936"; //Zuklappen
+            string Wert = TheValue.ToString(); //0 = Nichts Unternehmen, 1 = Energie Sparen
+            string Argumente = String.Format("/SETACVALUEINDEX {0} {1} {2} {3}", EnergieSchema, Untergruppe, Einstellung, Wert); //SETDCVALUEINDEX regelt nur Wechselstromberieb
+
+            //
+            var psi = new ProcessStartInfo("powercfg", Argumente);
+            psi.CreateNoWindow = true;
+            psi.UseShellExecute = false;
+            Process.Start(psi);
+        }
+    }
+}
